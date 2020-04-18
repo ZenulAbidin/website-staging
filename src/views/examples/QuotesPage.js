c2c80fa8f7eee40ed26310ab01cede30caeacdb5
Ali Sherief <alihsherief@linuxmail.org>
2020-04-19 00:25:27 +0200

    save files

--- src/views/examples/QuotesPage.js
+++ src/views/examples/QuotesPage.js
@@ -109,7 +109,7 @@ class QuotesPage extends React.Component {
       <>
         <IndexNavbar />
         <div className="wrapper">
-          <div className="section">
+          <div className="page-header" style={{"max-height": "none"}}>
             <img
               alt="..."
               className="dots"
@@ -120,6 +120,7 @@ class QuotesPage extends React.Component {
               className="path"
               src={require("assets/img/path4.png")}
             />
+            <br/>
             <div style={{"text-align": "center"}}>
               <h1 className="text-center">Miscellaneous quotes</h1>
               <p className="profile-description" className="text-center text-padding">

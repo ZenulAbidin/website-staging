14e9b732a3019498f5d884e4ee68ae849d167548
Ali Sherief <alihsherief@linuxmail.org>
2020-04-21 23:15:44 +0200

    save files

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -23,14 +23,7 @@ import { Container } from "reactstrap";
 class PageHeader extends React.Component {
   render() {
     return (
-      <div className="page-header header-filter">
-        <div className="squares square1" />
-        <div className="squares square2" />
-        <div className="squares square3" />
-        <div className="squares square4" />
-        <div className="squares square5" />
-        <div className="squares square6" />
-        <div className="squares square7" />
+      <div className="page-header header-filter background-1">
         <Container>
           <div className="content-center brand">
             <h1 className="h1-seo">BLK• React</h1>

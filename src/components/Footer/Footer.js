4789ef999fbe558374ec6bbbbdc44d838187c40b
Ali Sherief <alihsherief@linuxmail.org>
2020-04-22 02:55:34 +0200

    save files

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -107,8 +107,8 @@ class Footer extends React.Component {
             </Col>
           </Row>
         </Container>
-        <p>Template by <a href="https://demos.creative-tim.com/blk-design-system-react/#/">BLK• Design System React</a></p>
-        <p>Tips: bc1q4djl6pxt90nfs8fufdul26ufxukxxrczsfjj0h</p>
+        <p className="text-center">Template by <a href="https://demos.creative-tim.com/blk-design-system-react/#/">BLK• Design System React</a></p>
+        <p className="text-center">Tips: bc1q4djl6pxt90nfs8fufdul26ufxukxxrczsfjj0h</p>
       </footer>
     );
   }
